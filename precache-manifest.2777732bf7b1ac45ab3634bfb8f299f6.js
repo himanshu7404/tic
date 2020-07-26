fb29954bc1ba1f188011fa545d9b36a8cd6a771e
himanshu7404 <61195113+himanshu7404@users.noreply.github.com>
2020-07-26 15:57:05 +0530

    Updates

---
+++ precache-manifest.2777732bf7b1ac45ab3634bfb8f299f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d86d82ff58e8e29733de4d84846b83",
+    "url": "/tic/index.html"
+  },
+  {
+    "revision": "86c71e8f4a65267e2609",
+    "url": "/tic/static/css/main.6aaffb83.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c71e8f4a65267e2609",
+    "url": "/tic/static/js/main.d365a0b3.chunk.js"
+  },
+  {
+    "revision": "48976c7e4901a9219b93",
+    "url": "/tic/static/js/runtime-main.fce67067.js"
+  }
+]);
